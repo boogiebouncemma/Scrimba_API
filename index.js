e3903e2323e44a0c921c89008a9909c32c3ba48f
Boogie <85059214+boogiebouncemma@users.noreply.github.com>
2023-03-08 21:35:14 +0100

    adding fetch

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 
+const searchBtn = document.getElementById("search-button")
+let searchOutput = {}
+let movieCnt = document.getElementById("movie-container")
 
-fetch("http://www.omdbapi.com/?i=tt3896198&apikey=55634f5e")
-    .then(res => res.json())
-    .then(data => console.log(data))
+function getMovie(movie) {
+    `
+    <img src=${movie.poster} />
+    <h3>${movie.Title}</h3>
+    `
+}
+
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    let searchValue = document.getElementById("search-box").value
+
+    fetch(`http://www.omdbapi.com/?t=${searchValue}&apikey=55634f5e`)
+        .then(res => res.json())
+        .then(data => {
+            searchOutput = data
+        })
+
+    movieCnt.innerHTML = getMovie(searchOutput)
+    // how to do so that fetch is running when the value is available?
+    document.getElementById("search-box").value = ""
+    console.log(searchOutput)
+})
+
+
+
 
     
